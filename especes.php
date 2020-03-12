f7272bfc5f1b2e531dca26d5e87532bbe7b36415
Stanis Humez <stanis@netvolution.fr>
2020-03-12 17:29:23 +0100

    Intégration de la maquette HTML dansle PHP

--- especes.php
+++ especes.php
@@ -21,18 +21,26 @@
 
         $entetes = fgetcsv($fichier, 0, ",", "\"");
         while ($donnees = fgetcsv($fichier, 0, ",", "\"")) {
-
+            $nom = $donnees[1];
+            $nomlatin = $donnees[2];
+            $taille = $donnees[3];
+            $origine = $donnees[4];
+            $type = $donnees[5];
+            $facade = $donnees[6];
+            $image = $donnees[7];
         ?>
         <div class="col-md-6"><!-- debut fiche especes -->
             <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary">Type</strong>
-                    <h3 class="mb-0">Nom</h3>
-                    <div class="mb-1 text-muted">Nom latin</div>
-                    <p class="card-text mb-auto">Origine, façade, taille</p>
+                    <strong class="d-inline-block mb-2 text-primary"><?php echo $type; ?></strong>
+                    <h3 class="mb-0"><?php echo $nom; ?></h3>
+                    <div class="mb-1 text-muted"><?php echo $nomlatin; ?></div>
+                    <p class="card-text mb-auto">Origine : <?php echo $origine; ?><br>
+                        Façade : <?php echo $facade; ?><br>
+                        Taille : <?php echo $taille; ?></p>
                 </div>
                 <div class="col-auto d-none d-lg-block">
-                    <img src="https://www.mrgoodfish.com/wp-content/uploads/2017/11/Amande-de-me.png" class="bd-placeholder-img" width="200" height="125">
+                    <img src="<?php echo $image; ?>" class="bd-placeholder-img" width="200" height="125">
                 </div>
             </div>
         </div><!-- fin fiche espece -->
